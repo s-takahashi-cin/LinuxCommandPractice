af49cd6b17f7f10788e3f768adc4b4e2ca1b464c
s-takahashi-cin <s.takahashi.cingroup2012@gmail.com>
2024-03-06 22:42:11 -0800

    modified answer.sh

--- answer.sh
+++ answer.sh
@@ -4,15 +4,17 @@
 
 # 1. ターミナル/Gitbashを開いてください。(ここはコマンド実行は不要です。)
 # 2. ホームディレクトリに移動してください。(すでにホームディレクトリにいてもコマンドを実行して下さい)
+cd ~
 
 # 3. ホームディレクトリの中にある`geekationWorkspace`ディレクトリまで移動してください。(すでにオリエンテーションで作成済み)
-
+cd /Users/takahashisawa/geekation_orientation
 
 # 4. `LinuxCommandPractice`というディレクトリを作成して、`LinuxCommandPractice`に移動してください。
-
+mkdir LinuxCommandPractice
+cd LinuxCommandPractice
 
 # 5. カレントディレクトリのパスを表示してください。
-
+pwd
 
 # 6. 次に、下の画像のようにGithubで`LinuxCommandPractice`という名前のリポジトリを作成してください。
 # 以下のGithubリポジトリ作成時の初期コマンドは書き換えなくてOKです。7に進んでください。
@@ -21,9 +23,9 @@ $ echo "# LinuxCommandPractice" >> README.md
 $ git init
 $ git add README.md
 $ git commit -m "first commit"
-$ git branch linux-command-practice //ブランチ名作ってしまいました；；
+$ git branch -M main
 $ git remote add origin https://github.com/s-takahashi-cin/LinuxCommandPractice.git
-$ git push -u origin linux-command-practice
+$ git push -u origin main
 # ここまでGithubリポジトリ作成時の初期コマンド
 
 # 7. `LinuxCommandPractice`ディレクトリに`.git`ディレクトリが表示されていることを確認してください。
@@ -36,7 +38,7 @@ ls -a
 ls 
 
 # 10. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`practice_dir_1`内に`practice_dir_2`というディレクトリを作成してください。
- mkdir LinuxCommandPractice/practice_dir_1/practice_dir_2
+ mkdir practice_dir_1/practice_dir_2
 
 # 11. `LinuxCommandPractice`ディレクトリ内から**移動せずに**、`practice_dir_1`内に`practice_dir_2`ディレクトリが作成されていることを確認(表示)してください。
 ls practice_dir_1/
@@ -68,7 +70,7 @@ cd ..
 rm /Users/takahashisawa/geekation_orientation/LinuxCommandPractice/practice_file2.txt 
 
 # 20. `vi`または`vim`を用いて、カレントディレクトリにある`practice_file1.txt`に`I'm good at Linux commands!`と書き込んでください。
- vi practice_file1.txt
+vi practice_file1.txt
 
 # 21. `practice_file1.txt`に書き込んだ内容を出力してください。
 cat practice_file1.txt
